cf7c5f9083a5bb35181a337edb35db63e5a44196
VSat08 <satyam2001verma@gmail.com>
2024-05-03 00:01:57 +0530

    Recursion started

---
+++ Fundamentals/Recursion/Exponent.java
@@ -0,0 +1,23 @@
+package Fundamentals.Recursion;
+
+// print x^n(stack heigh =n)
+
+public class Exponent {
+
+    public static int calcPower(int x, int n) {
+
+        if (x == 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+
+        return x * calcPower(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int x = 3, n = 4;
+        System.out.println(calcPower(x, n));
+    }
+}
---
+++ Fundamentals/Recursion/OPTIMISED_Exponent.java
@@ -0,0 +1,27 @@
+package Fundamentals.Recursion;
+// print x^n(stack heigh =logn)
+
+public class OPTIMISED_Exponent {
+    public static int calcOptmisedPower(int x, int n) {
+
+        if (x == 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n % 2 == 0) {
+            return calcOptmisedPower(x, n / 2) * calcOptmisedPower(x, n / 2);
+        } else {
+            return x * calcOptmisedPower(x, n/2) * calcOptmisedPower(x, n / 2);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int x = 2, n = 5;
+        System.out.println(calcOptmisedPower(x, n));
+    }
+}
